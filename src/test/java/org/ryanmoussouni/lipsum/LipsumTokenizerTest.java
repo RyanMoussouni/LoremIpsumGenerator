06637f0cf32f9db17ea65886136b5bcd55e9e77c
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-10-07 15:56:27 +0200

    Add LipsumTokenizerTest
    The setUp and the tearDown

--- src/test/java/org/ryanmoussouni/lipsum/LipsumTokenizerTest.java
+++ src/test/java/org/ryanmoussouni/lipsum/LipsumTokenizerTest.java
@@ -1,4 +1,30 @@
+package org.ryanmoussouni.lipsum;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class LipsumTokenizerTest {
-  
+
+    private LipsumTokenizer tokenizer;
+
+    @BeforeEach
+    void setUp() {
+        try {
+            tokenizer = new LipsumTokenizer();
+        } catch (TokenizationException te) {
+            Assertions.fail("Could not instanciate the tokenizer");
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        try {
+            tokenizer.tearDown();
+        } catch (TokenizationException e) {
+            Assertions.fail("Could not teardown the tokenizer");
+        }
+    }
 }
