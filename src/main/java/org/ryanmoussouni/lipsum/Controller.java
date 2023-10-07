30fd8a8d81fdf79e9e7193113b1a36681ae42ed8
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-10-07 15:07:02 +0200

    Add controller

---
+++ src/main/java/org/ryanmoussouni/lipsum/Controller.java
@@ -0,0 +1,17 @@
+package org.ryanmoussouni.lipsum;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping(path="lipsum", method = RequestMethod.GET)
+    public ResponseEntity<String> handleTextGenerationRequest() {
+        var body = "test";
+        var status = HttpStatus.OK;
+        return new ResponseEntity<>(body, status);
+    }
+}
