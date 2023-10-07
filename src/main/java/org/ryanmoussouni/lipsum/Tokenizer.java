d27ed8ddd2a6c70ce8c08324ef11f8e57ebe853a
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-10-07 17:52:25 +0200

    Add component annotations
    To the tokenizer and the Vocab Data Source

--- src/main/java/org/ryanmoussouni/lipsum/Tokenizer.java
+++ src/main/java/org/ryanmoussouni/lipsum/Tokenizer.java
@@ -1,7 +1,10 @@
 package org.ryanmoussouni.lipsum;
 
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
+@Component
 public interface Tokenizer {
     List<String> tokenize() throws TokenizationException;
 }
--- src/main/java/org/ryanmoussouni/lipsum/VocabularySource.java
+++ src/main/java/org/ryanmoussouni/lipsum/VocabularySource.java
@@ -1,7 +1,10 @@
 package org.ryanmoussouni.lipsum;
 
+import org.springframework.stereotype.Component;
+
 import java.util.Set;
 
+@Component
 public interface VocabularySource {
     Set<String> getVocabulary() throws VocabularyCreationException;
 }
