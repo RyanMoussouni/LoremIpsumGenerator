29c8c063491d44f3d58e9ef11b25963b55b401dc
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-10-07 15:01:05 +0200

    First commit

---
+++ src/main/java/org/ryanmoussouni/lipsum/LipsumApplication.java
@@ -0,0 +1,13 @@
+package org.ryanmoussouni.lipsum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LipsumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LipsumApplication.class, args);
+	}
+
+}
