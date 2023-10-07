c373e92ba423c949896b642393fe640057ecd0d9
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-10-07 15:09:27 +0200

    Add controller test

---
+++ src/test/java/org/ryanmoussouni/lipsum/ControllerTest.java
@@ -0,0 +1,13 @@
+package org.ryanmoussouni.lipsum;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class ControllerTest {
+    @Test
+    void handleTextGenerationRequest_anyRequest_returnsSuccessfulResponse() {
+    }
+}
