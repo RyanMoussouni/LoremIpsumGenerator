c9249cb955d5ab7e7efa43216b4d859f9cfc1553
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-10-07 15:24:53 +0200

    Add Vocabulary Source

---
+++ src/main/java/org/ryanmoussouni/lipsum/CiceroExtract.java
@@ -0,0 +1,10 @@
+package org.ryanmoussouni.lipsum;
+
+import java.util.Set;
+
+public class CiceroExtract implements VocabularySource {
+    @Override
+    public Set<String> getVocabulary() {
+        return null;
+    }
+}
---
+++ src/main/java/org/ryanmoussouni/lipsum/VocabularySource.java
@@ -0,0 +1,7 @@
+package org.ryanmoussouni.lipsum;
+
+import java.util.Set;
+
+public interface VocabularySource {
+    Set<String> getVocabulary();
+}
