6386c7bd4c4042f5ead92ed1e9f85381c71c7164
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-10-07 17:04:48 +0200

    Add test to the Tokenizer

--- src/test/java/org/ryanmoussouni/lipsum/LipsumTokenizerTest.java
+++ src/test/java/org/ryanmoussouni/lipsum/LipsumTokenizerTest.java
@@ -1,4 +1,30 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.ryanmoussouni.lipsum;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 class LipsumTokenizerTest {
-  
+
+    @Test
+    void tokenize_singleSentence_returnsAllTokensInOrder() throws IOException, TokenizationException {
+        var mockInputStream = mock(InputStream.class);
+        var sentence = "Sed ut perspiciatis, unde omnis iste natus error.";
+        var sentenceAsBytes = sentence.getBytes(StandardCharsets.UTF_8);
+        when(mockInputStream.readAllBytes())
+                .thenReturn(sentenceAsBytes);
+        var tokenizer = new LipsumTokenizer(mockInputStream);
+        var expectedTokens = List.of("sed", "ut", "perspiciatis", "unde", "omnis", "iste", "natus", "error");
+
+        var actualTokens = tokenizer.tokenize();
+
+        Assertions.assertArrayEquals(expectedTokens.toArray(), actualTokens.toArray());
+    }
 }
