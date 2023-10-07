6849d05a2ef4a60cb2671d5c1c8de2dc52450098
Ryan Moussouni <moussouni.ryan0@gmail.com>
2023-10-07 18:44:13 +0200

    Update TextParameters

---
+++ src/main/java/org/ryanmoussouni/lipsum/LoremIpsumGenerator.java
@@ -0,0 +1,2 @@
+package org.ryanmoussouni.lipsum;public class LoremIpsumGenerator {
+}
--- src/main/java/org/ryanmoussouni/lipsum/TextParameters.java
+++ src/main/java/org/ryanmoussouni/lipsum/TextParameters.java
@@ -1,11 +1,16 @@
 package org.ryanmoussouni.lipsum;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.Set;
 
+@AllArgsConstructor
 @Getter
 public class TextParameters {
-    private Integer sentenceSize;
-    private Set<Character> punctuationCharacters;
+    private final Integer sentenceSize;
+    private final Set<Character> punctuationCharacters;
+    private final Integer sentenceCount;
+    private final Float punctuationToVocabWordsProportion;
+    private final Float commaToSemiColonProportion;
 }
